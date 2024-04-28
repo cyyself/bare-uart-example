acafb15526f81029b26a32f3672987d2baa0b95d
Yangyu Chen <cyy@cyyself.name>
2024-04-28 17:35:14 +0800

    test multicore

--- main.c
+++ main.c
@@ -2,11 +2,28 @@
 #include "trap.h"
 #include "mmu.h"
 
+volatile int hart_fin[2];
+
 int main(long hartid) {
     // setup_mtvec();
     // mmu_init();
     // enter_smode();
-    print_s("Hello World from UART!\n");
+    int wait = 0;
+    do {
+        wait = 0;
+        for (int i=0;i<hartid;i++) {
+            if (hart_fin[i] == 0) {
+                print_s("Waiting for hart ");
+                print_long(i);
+                print_s("\r\n");
+                wait = 1;
+                break;
+            }
+        }
+    } while(wait);
+    print_s("Hello World from hart ");
     print_long(hartid);
+    print_s("\r\n");
+    hart_fin[hartid] = 1;
     return 0;
 }
