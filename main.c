f045874e75d7585fedd75a7b3f36e623cc6ab3fd
Yangyu Chen <cyy@cyyself.name>
2024-05-01 03:01:07 +0800

    test zicond for xs

--- main.c
+++ main.c
@@ -4,6 +4,37 @@
 
 volatile int hart_fin[2];
 
+void test_zicond() {
+    long x = 0xabcddeadbeef;
+    long y_0 = 0;
+    long y_1 = 1;
+    long res0, res1, res2, res3;
+    asm volatile(
+        "czero.eqz %0, %1, %2"
+        : "=r" (res0)
+        : "r" (x), "r" (y_0)
+    );
+    asm volatile(
+        "czero.eqz %0, %1, %2"
+        : "=r" (res1)
+        : "r" (x), "r" (y_1)
+    );
+    asm volatile(
+        "czero.nez %0, %1, %2"
+        : "=r" (res2)
+        : "r" (x), "r" (y_0)
+    );
+    asm volatile(
+        "czero.nez %0, %1, %2"
+        : "=r" (res3)
+        : "r" (x), "r" (y_1)
+    );
+    dump_hex(res0);
+    dump_hex(res1);
+    dump_hex(res2);
+    dump_hex(res3);
+}
+
 int main(long hartid) {
     // setup_mtvec();
     // mmu_init();
@@ -25,5 +56,6 @@ int main(long hartid) {
     print_long(hartid);
     print_s("\r\n");
     hart_fin[hartid] = 1;
+    test_zicond();
     return 0;
 }
