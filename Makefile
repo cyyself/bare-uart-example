cyyself/simple-sw-workbench

turn on fs and vs

Makefile
@@ -3,7 +3,7 @@ CC		:=	$(CROSS_COMPILE)gcc
 OBJCOPY	:=	$(CROSS_COMPILE)objcopy
 OBJDUMP :=  $(CROSS_COMPILE)objdump
 EXTRA_CFLAGS := 
-CFLAGS := -nostdlib -static -mcmodel=medany -fno-builtin-printf -O0 $(EXTRA_CFLAGS)
+CFLAGS := -march=rv64gcv -nostdlib -static -mcmodel=medany -fno-builtin-printf -O0 $(EXTRA_CFLAGS)
 .PHONY: clean
 
 all: start.bin dump.S

cyyself/bare-uart-example

Makefile
@@ -2,8 +2,8 @@ CROSS_COMPILE :=	riscv64-linux-gnu-
 CC		:=	$(CROSS_COMPILE)gcc
 OBJCOPY	:=	$(CROSS_COMPILE)objcopy
 OBJDUMP :=  $(CROSS_COMPILE)objdump
-EXTRA_CFLAGS := -march=rv64gcv_zicond_zba
-CFLAGS := -nostdlib -static -mcmodel=medany -fno-builtin-printf -O0 $(EXTRA_CFLAGS)
+EXTRA_CFLAGS := 
+CFLAGS := -march=rv64gcv_zicond_zba -nostdlib -static -mcmodel=medany -fno-builtin-printf -O0 $(EXTRA_CFLAGS)
 .PHONY: clean
 
 all: start.bin dump.S